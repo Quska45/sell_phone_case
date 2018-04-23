2e1d330fef65ac54421df5f3a4f5aa74c0addfcb
qusrh <qusrh@DESKTOP-DRG9D7J>
2018-04-23 23:48:19 +0900

    2018.04.23

--- sellphone_case2/src/com/gogh/action/BoardFavoriteAction.java
+++ sellphone_case2/src/com/gogh/action/BoardFavoriteAction.java
@@ -19,10 +19,12 @@ public class BoardFavoriteAction implements Action {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		//게시글 상세 페이지에서 보내준 bno를 변수에 담았다.
 		Integer bno = Integer.parseInt(request.getParameter("bno"));
 		System.out.println("bno " + bno);
 		
 		//세션을 활용한 조회수 증가 방지
+		//세션 객체를 하나 생성하고 이것을 이용해서(세션 좋아요가 클릭된 시간을 저장하고 다시 좋아요가 눌렸을 때의 시간과 비교하는 방식)
 		HttpSession session = request.getSession();
 		
 		BoardDAO bDao = BoardDAO.getInstance();
